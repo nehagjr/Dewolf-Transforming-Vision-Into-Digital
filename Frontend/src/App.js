396e992714a127b7a6911ee87b556221cb8714bc
Neha patel <pneha3169@gmail.com>
2024-09-21 16:37:16 +0530

    Create About Service and Priceing page with css

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,8 +1,11 @@
+import Service from "./User_component/Services";
+import Pricing from "./User_component/Pricing";
 const App = ()=>{
   return(
     <>
     <h1>This is app</h1>
-    
+    <Service/>
+    <Pricing/>
     </>
   )
 }
