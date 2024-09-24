57c6687dc1f36f3e9d9ca22f131fd1924d9451c0
Neha patel <pneha3169@gmail.com>
2024-09-24 17:37:56 +0530

    routing

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,3 +1,34 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Common_Components/Home";
 import Layout from "./Common_Components/Layout";
