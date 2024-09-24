1b756c97351b3e4e38d1a5b88af92b883b499cde
Neha patel <pneha3169@gmail.com>
2024-09-24 18:01:19 +0530

    routing of common page

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,34 +1,3 @@
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Common_Components/Home";
 import Layout from "./Common_Components/Layout";
