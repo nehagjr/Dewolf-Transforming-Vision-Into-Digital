ce3d12fd20f80f4577ce3ffcffe71a2765680ed8
siddharth <badkursiddharth282@gmail.com>
2024-09-30 13:03:18 +0530

    resolve merge conflict

--- Frontend/src/Common_Components/Contact.js
+++ Frontend/src/Common_Components/Contact.js
@@ -2,48 +2,6 @@ import contact from '../assets/images/2.jpg'
 const Contect = ()=>{
     return(
       <>
-<<<<<<< HEAD
-      <section class="contact" id="contact">
-        <div class="container contact1">
-          <div class="contact-content">
-            <figure class="contact-banner">
-              <img src={contact} alt="contact page"/>
-            </figure>
-          </div> 
-        <form action="" class="contact-form">
-            <div class="input-wrapper">
-              <label for="name" class="input-label">Name *</label>
-
-              <input type="text" name="name" id="name" required placeholder="Type Name" class="input-field"/>
-            </div>
-
-            <div class="input-wrapper">
-              <label for="phone" class="input-label">Phone</label>
-
-              <input type="tel" name="phone" id="phone" placeholder="Type Phone Number" class="input-field"/>
-            </div>
-
-            <div class="input-wrapper">
-              <label for="email" class="input-label">Email Address *</label>
-
-              <input type="email" name="email" id="email" required placeholder="Type Email Address" class="input-field"/>
-            </div>
-
-            <div class="input-wrapper">
-              <label for="message" class="input-label">How can we help? *</label>
-
-              
-            </div>
-
-            <button type="submit" class="btn btn-primary">Send Message</button>
-
-          </form>
-
-        </div>
-      </section>
-=======
-
->>>>>>> 24a87e90f05da92a68b79f518bd6a02be971fe51
    
       </>
     )
