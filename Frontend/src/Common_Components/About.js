0caec5813a5e1020b71d565adb613b676d84d95f
mehak <mehakpatelattitude03@gmail.com>
2024-10-01 16:44:24 +0530

    add about page

--- Frontend/src/Common_Components/About.js
+++ Frontend/src/Common_Components/About.js
@@ -1,10 +1,66 @@
 
-
+import '../assets/css/about.css'
 
 const About = ()=>{
     return(
      <>
-     hiiii
+     <section id="about" class="about section">
+  
+  <div class="container" data-aos="fade-up" data-aos-delay="100">
+    <div class="row align-items-xl-center gy-5">
+
+      <div class="col-xl-5 content">
+        <h3>About Us</h3>
+        <h2>Regularly Optimize Your Website for SEO and Conversions</h2>
+        <p>Digital marketing promotes products and services through channels such as websites, mobile devices, and social media platforms.Digital marketers have a number of tools to measure the effectiveness of their campaigns.</p>
+        <a href="https://www.digitalsilk.com/digital-marketing/" class="read-more"><span>Read More</span><i class="bi bi-arrow-right"></i></a>
+      </div>
+
+      <div class="col-xl-7">
+        <div class="row gy-4 icon-boxes">
+
+          <div class="col-md-6" data-aos="fade-up" data-aos-delay="200">
+            <div class="icon-box">
+            <i class="fa-solid fa-chart-simple"></i>
+              <h3>Affialite Marketing</h3>
+              <p>Magni repellendus vel ullam hic officia accusantium ipsa dolor omnis dolor voluptatem</p>
+            </div>
+          </div>
+
+          <div class="col-md-6" data-aos="fade-up" data-aos-delay="300">
+            <div class="icon-box">
+            <i class="fa-solid fa-chart-line"></i>
+              <h3>AD campaigns</h3>
+              <p>Autem saepe animi et aut aspernatur culpa facere. Rerum saepe rerum voluptates quia</p>
+            </div>
+          </div>
+
+          <div class="col-md-6" data-aos="fade-up" data-aos-delay="400">
+            <div class="icon-box">
+            <i class="fa-solid fa-building"></i>
+              <h3>Competitor Research </h3>
+              <p>Omnis perferendis molestias culpa sed. Recusandae quas possimus. Quod consequatur corrupti</p>
+            </div>
+          </div>
+
+          <div class="col-md-6" data-aos="fade-up" data-aos-delay="500">
+            <div class="icon-box">
+            <i class="fa-solid fa-chart-column"></i>
+              <h3>Content Curation</h3>
+              <p>Sint et dolor voluptas minus possimus nostrum. Reiciendis commodi eligendi omnis quideme lorenda</p>
+            </div>
+          </div>
+
+        </div>
+      </div>
+
+    </div>
+  </div>
+
+</section>
+
+
+  
      </>
     )
   }
